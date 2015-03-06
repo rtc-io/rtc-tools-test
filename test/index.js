99e44df5b6581a738b0aaf3ea283aa0801620880
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-06 16:46:34 +1100

    Imported tests from rtc-tools and rtc-signaller-testrun

---
+++ test/index.js
@@ -0,0 +1,15 @@
+var detect = require('rtc-core/detect');
+
+module.exports = function(tools, createSignaller, opts) {
+  require('./coupling')(tools, createSignaller, opts);
+  require('./coupling-constraints')(tools, createSignaller, opts);
+  require('./capture-close-localonly')(tools, createSignaller, opts);
+
+  if (! detect.moz) {
+    require('./coupling-reactive')(tools, createSignaller, opts);
+    require('./coupling-reactive-randomdelay')(tools, createSignaller, opts);
+    require('./coupling-reactive-randomdelaystreams')(tools, createSignaller, opts);
+
+    require('./capture-close')(tools, createSignaller, opts);
+  }
+};
